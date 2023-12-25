cf1ef9552b56b10444d0b0d4b5cf87846b996864
armzig <bellotaophyc@outlook.com>
2023-12-25 07:35:49 +0100

    using the request method to access form data. app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ render_template, request, redirect, url_for
 import os # importing operating system module
 
 app = Flask(__name__)
+# to stop caching static file
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/') # this decorator create the home route
 def home():
@@ -18,10 +20,19 @@ def about():
     name = 'Cloud Engineering Acts'
     return render_template('about.html', name = name, title = 'About Us')
 
-@app.route('/post')
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+@app.route('/post', methods= ['GET','POST'])
 def post():
     name = 'Text Analyzer'
-    return render_template('post.html', name = name, title = name)
+    if request.method == 'GET':
+         return render_template('post.html', name = name, title = name)
+    if request.method =='POST':
+        content = request.form['content']
+        print(content)
+        return redirect(url_for('result'))
 
 if __name__ == '__main__':
     # for deployment we use the environ
